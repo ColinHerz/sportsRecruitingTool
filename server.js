53e916b8ff251db214596ef822a58de4e260cc31
DESKTOP-5J1A2PI\colin <ColinHerzberg21@gmail.com>
2020-03-04 11:22:16 -0500

    I think this is it ?

--- server.js
+++ server.js
@@ -43,10 +43,9 @@ app.get('/testpoint', (req, res) => {
 //app.post('/contacts/delete/:id', routes.postContactsDelete);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-
+  app.use(express.static('frontend/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
   });
 }
 
