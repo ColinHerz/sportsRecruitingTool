624a2a5dfca92f1ae5f0bc13d17a28006d1a7352
DESKTOP-5J1A2PI\colin <ColinHerzberg21@gmail.com>
2020-03-04 20:32:05 -0500

    changed package.json

--- server.js
+++ server.js
@@ -43,7 +43,8 @@ app.get('/testpoint', (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+    console.log(path.join(__dirname, '/build/index.html'));
+    res.sendFile(path.join(__dirname, '/build/index.html'))
   });
 }
 
