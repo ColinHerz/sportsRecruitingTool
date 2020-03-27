3e14cc5f0b16cd8b83b7b6876aa82179b94414cf
Goff-Davis <davispgoff@gmail.com>
2020-03-27 19:09:45 -0400

    Add basic html for log in modal

--- src/App.js
+++ src/App.js
@@ -9,12 +9,15 @@ import Event from "./components/event/Event.js";
 import Footer from "./components/footer/Footer.js";
 import Header from "./components/header/Header.js";
 import Hero from "./components/hero/Hero.js";
+import LoginModal from "./components/login-modal/LoginModal.js";
 import Profile from "./components/profile/Profile.js";
 
 const App = props => {
 	return (
 		<Router>
 			<Header />
+
+			<LoginModal isRegistering={true} />
 
 			<Switch>
 				<Route path="/profile/:id/">
