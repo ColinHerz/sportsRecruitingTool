cb055811efc072c2c7cc929978d5223121400205
beniliff <bengiliff@gmail.com>
2020-04-14 23:01:11 -0400

    More Conflicts

--- server.js
+++ server.js
@@ -35,10 +35,6 @@ mongoose
 var routes = require('./routes');
 
 // User get login register and verification
-app.get('/api/users/login', routes.getUserLogin);
-
-
-// User get login register and verification
 app.post('/api/users/login', routes.postUserLogin);
 
 app.post('/api/users/register', routes.postUserRegister);
