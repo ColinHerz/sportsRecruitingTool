834f1a0085a7ebc9f2c4ab09ac7797e0c821c3ce
Goff-Davis <davispgoff@gmail.com>
2020-04-17 19:35:34 -0400

    Add outline of match view

---
+++ src/components/viewMatch/ViewMatch.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const ViewMatch = props => {
+	const { mid } = useParams;
+
+	return (
+		<main id="view-match">
+		</main>
+	);
+};
+
+export default ViewMatch;
